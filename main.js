22412e7e030a732d0bb124e0f0e797688e9d7855
siva <sivashakthiarun.c@gmail.com>
2022-09-21 10:52:39 +0530

    repush

---
+++ main.js
@@ -0,0 +1,20 @@
+/* factorial  */
+var i ;
+fact = 1;
+for (i = 1; i <= 4; i++) {
+    fact = fact * i;
+}
+console.log(fact);
+
+
+
+/* factorial series */
+/*
+var i ;
+fact = 1;
+for (i = 1; i <= 4; i++) {
+    fact = fact * i;
+    console.log(fact);
+}
+*/
+
